676884b74adff41ce72d6c549e305154f1c42683
Zhangdongbo <zhang.dongbo@cctvnews.cn>
2018-12-10 16:08:01 +0800

    增加简单使用说明及截图

--- search/src/main/java/org/fran/wiki/search/Test.java
+++ search/src/main/java/org/fran/wiki/search/Test.java
@@ -1,8 +1,5 @@
 package org.fran.wiki.search;
 
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.RAMDirectory;
-
 public class Test {
 
     public static void main(String[] args){
